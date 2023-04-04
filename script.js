ab8109a161dc0d968d129f0a8e34a5a2501b127c
calingoia2001 <calingoia01@yahoo.com>
2023-04-04 19:34:34 +0300

    async function script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("test");
+const apiKey = "c8853f099bc8a6740c33132d97e0f47f";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function weather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+}
+
+weather("Cluj-Napoca");
